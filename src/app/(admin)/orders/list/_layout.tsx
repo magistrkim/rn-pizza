79cf7ca524d741f6e008419ce1c8ad563e52adb6
Nataliya Kim <magistr1982@gmail.com>
2024-08-08 13:00:49 +0300

    Install Supabase into project

--- src/app/(admin)/orders/list/_layout.tsx
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+
 import React from "react";
 import { withLayoutContext } from "expo-router";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";

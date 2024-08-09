1a13d6814826f97f0398569b2222be305a10c6cf
Nataliya Kim <magistr1982@gmail.com>
2024-08-09 15:55:56 +0300

    Add SignOut Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,19 @@
-import { View } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 import React from "react";
 import Button from "../components/Button";
-import { Link } from "expo-router";
+import { Link, Redirect } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
+import { supabase } from "@/lib/supabase";
 
 const index = () => {
+  const { session, loading } = useAuth();
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+  if (!session) {
+    return <Redirect href={"/sign-in"} />;
+  }
   return (
     <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
       <Link href={"/(user)"} asChild>
@@ -13,8 +23,9 @@ const index = () => {
         <Button text="Admin" />
       </Link>
       <Link href={"/sign-up"} asChild>
-        <Button text="Sign-up" />
+        <Button text="Sign up" />
       </Link>
+      <Button text="Sign out" onPress={() => supabase.auth.signOut()} />
     </View>
   );
 };
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,28 +1,45 @@
 import { supabase } from "@/lib/supabase";
 import { Session } from "@supabase/supabase-js";
-import { createContext, PropsWithChildren, useEffect, useState } from "react";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 type AuthData = {
   session: Session | null;
+  loading: boolean;
 };
 
 const AuthContext = createContext<AuthData>({
   session: null,
+  loading: true,
 });
 
 const AuthProvider = ({ children }: PropsWithChildren) => {
   const [session, setSession] = useState<Session | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchSession = async () => {
       const { data } = await supabase.auth.getSession();
       setSession(data.session);
+      setLoading(false);
     };
     fetchSession();
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ session, loading }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 export default AuthProvider;
+
+export const useAuth = () => useContext(AuthContext);

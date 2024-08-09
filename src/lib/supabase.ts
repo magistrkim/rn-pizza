d862ab2d81310b1b0a4390374cb81bd1b35c9514
Nataliya Kim <magistr1982@gmail.com>
2024-08-09 15:38:18 +0300

    Delete Keys from supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,10 +2,6 @@ import "react-native-url-polyfill/auto";
 import * as SecureStore from "expo-secure-store";
 import { createClient } from "@supabase/supabase-js";
 
-// const URL = "https://tzbgxacnohjompoimcmz.supabase.co";
-// const KEY =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR6Ymd4YWNub2hqb21wb2ltY216Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxMTEwNjksImV4cCI6MjAzODY4NzA2OX0.x4fjFW3a9l5dJhuvMl9QiOkpIZ2oYKBnDro7VCmvHY0";
-
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
     return SecureStore.getItemAsync(key);

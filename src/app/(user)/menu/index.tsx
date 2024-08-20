7e05296de61be75cf0e43c7cd3fab8a87c8ef451
Nataliya Kim <magistr1982@gmail.com>
2024-08-20 08:49:59 +0300

    Create useProductList hook

--- src/app/(user)/menu/index.tsx
+++ src/app/(user)/menu/index.tsx
@@ -6,24 +6,10 @@ import {
   Text,
 } from "react-native";
 import ProductListItem from "@/components/ProductListItem";
-import { supabase } from "@/lib/supabase";
-import { useQuery } from "@tanstack/react-query";
+import { useProductList } from "@/api/products";
 
 const MenuScreen = () => {
-  const {
-    data: products,
-    error,
-    isLoading,
-  } = useQuery({
-    queryKey: ["products"],
-    queryFn: async () => {
-      const { data, error } = await supabase.from("products").select("*");
-      if (error) {
-        throw new Error(error.message);
-      }
-      return data;
-    },
-  });
+  const { data: products, error, isLoading } = useProductList();
 
   if (isLoading) {
     return <ActivityIndicator />;

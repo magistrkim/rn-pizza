2aca1cdad73d968bc46adbaf733e58bc80ec7c6e
Nataliya Kim <magistr1982@gmail.com>
2024-08-20 08:56:25 +0300

    Add useProductList hook in user and admin side

--- src/app/(admin)/menu/index.tsx
+++ src/app/(admin)/menu/index.tsx
@@ -2,11 +2,21 @@ import {
   StyleSheet,
   View,
   FlatList,
+  ActivityIndicator,
+  Text,
 } from "react-native";
 import ProductListItem from "@/components/ProductListItem";
-import products from "@assets/data/products";
+import { useProductList } from "@/api/products";
 
 const MenuScreen = () => {
+  const { data: products, error, isLoading } = useProductList();
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
+    return <Text>Failed to fetch products</Text>;
+  }
   return (
     <View>
       <FlatList

3f0993a42580053e60ff78116807474551165612
Nataliya Kim <magistr1982@gmail.com>
2024-08-23 06:13:29 +0300

    Create useUpdateProduct hook

--- src/api/products/index.ts
+++ src/api/products/index.ts
@@ -53,3 +53,28 @@ export const useInsertProduct = () => {
     },
   });
 };
+export const useUpdateProduct = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    async mutationFn(data: any) {
+      const { data: updatedProduct, error } = await supabase
+        .from("products")
+        .update({
+          name: data.name,
+          price: data.price,
+          image: data.image,
+        })
+        .eq("id", data.id)
+        .select()
+        .single();
+      if (error) {
+        throw new Error(error.message);
+      }
+      return updatedProduct;
+    },
+    async onSuccess(_, data) {
+      await queryClient.invalidateQueries({ queryKey: ["products"] });
+      await queryClient.invalidateQueries({ queryKey: ["products", data.id] });
+    },
+  });
+};

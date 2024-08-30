b606d1cf673826d48cb7b8474b8ca814a9bb01af
Nataliya Kim <magistr1982@gmail.com>
2024-08-30 11:44:51 +0300

    Create useInsertOrder hook

--- src/api/orders/index.ts
+++ src/api/orders/index.ts
@@ -1,6 +1,7 @@
 import { supabase } from "@/lib/supabase";
 import { useAuth } from "@/providers/AuthProvider";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { InsertTables } from "@/types";
 
 export const useAdminOrdersList = ({ archived = false }) => {
   const activeStatus = archived ? ["Done"] : ["New", "Cooking", "Delivering"];
@@ -49,6 +50,26 @@ export const useOrderById = (id: number) => {
         throw new Error(error.message);
       }
       return data;
+    },
+  });
+};
+export const useInsertOrder = () => {
+  const queryClient = useQueryClient();
+  const { session } = useAuth();
+  const userId = session?.user.id;
+  return useMutation({
+    async mutationFn(data: InsertTables<"orders">) {
+      const { data: newOrder, error } = await supabase
+        .from("orders")
+        .insert({ ...data, user_id: userId })
+        .single();
+      if (error) {
+        throw new Error(error.message);
+      }
+      return newOrder;
+    },
+    async onSuccess() {
+      await queryClient.invalidateQueries({ queryKey: ["orders"] });
     },
   });
 };
